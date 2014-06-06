2cfa1206f302c5a4fdd4985673ced6d5b4983b59
Mr Xu <11302010019@fudan.edu.cn>
2014-06-07 02:58:04 +0800

    almost finish my job
    dictionary.txt is the dictionary
    settings store settings' initial value

--- WordMaster/src/PropertyValue.java
+++ WordMaster/src/PropertyValue.java
@@ -1,4 +1,34 @@
+import java.io.*;
+import java.util.*;
 
 public class PropertyValue {
-
+     public static void main(String args[]){
+    	 int count[] = new int[26];
+    	 for(int i=0;i<26;i++){
+    		 count[i] = 0;
+    	 }
+    	 try {
+             String encoding="GBK";
+             File file=new File("C:\\Users\\admin\\Documents\\GitHub\\WordMaster\\WordMaster\\src\\dictionary.txt");
+             if(file.isFile() && file.exists()){ //判断文件是否存在
+                 InputStreamReader read = new InputStreamReader(new FileInputStream(file),encoding);//考虑到编码格式
+                 BufferedReader bufferedReader = new BufferedReader(read);
+                 String lineTxt = null;
+                 while((lineTxt = bufferedReader.readLine()) != null){
+                     count[lineTxt.charAt(0)-'a']++;
+                 }
+                 read.close();
+     }else{
+         System.out.println("找不到指定的文件");
+     }
+     } catch (Exception e) {
+         System.out.println("读取文件内容出错");
+         e.printStackTrace();
+     }
+    	 
+    	 for(int i=0;i<26;i++){
+    		 char c = (char) ('a'+i);
+    		 System.out.println(c + ":"+count[i]);
+    	 }
+     }
 }
